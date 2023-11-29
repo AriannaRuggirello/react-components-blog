f214659c782e994f8c8127a5316a03899f67aa4b
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-29 17:40:36 +0100

    print json arrays

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
-      <footer className="bg-zinc-800 text-white py-4 text-center">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque distinctio reiciendis nesciunt id at similique odit beatae eveniet iste blanditiis maiores vitae cum sapiente harum commodi ipsum obcaecati, odio neque.
+      <footer className="bg-orange-300 text-white py-4 text-center">
+       footer
       </footer>
     )
 }
